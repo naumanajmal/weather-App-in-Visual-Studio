b326e590d0178bc97c6792b7b483483946a52b10
naumanajmal <naumanajmal78@gmail.com>
2021-11-14 11:18:39 +0500

    files Added

---
+++ Mahdi Bu-Ali_Lab9_10_164/Mahdi Bu-Ali_Lab9_10_164/WeatherAp.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Mahdi_Bu_Ali_Lab11_164
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.dateLabel = new System.Windows.Forms.Label();
+            this.precipitationLabel = new System.Windows.Forms.Label();
+            this.highTempLabel = new System.Windows.Forms.Label();
+            this.lowTempLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Location = new System.Drawing.Point(40, 43);
+            this.dateTimePicker.MaxDate = new System.DateTime(2018, 1, 31, 0, 0, 0, 0);
+            this.dateTimePicker.MinDate = new System.DateTime(2018, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(261, 20);
+            this.dateTimePicker.TabIndex = 0;
+            this.dateTimePicker.Value = new System.DateTime(2018, 1, 31, 0, 0, 0, 0);
+            this.dateTimePicker.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // dateLabel
+            // 
+            this.dateLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dateLabel.Location = new System.Drawing.Point(188, 85);
+            this.dateLabel.Name = "dateLabel";
+            this.dateLabel.Size = new System.Drawing.Size(100, 30);
+            this.dateLabel.TabIndex = 1;
+            // 
+            // precipitationLabel
+            // 
+            this.precipitationLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.precipitationLabel.Location = new System.Drawing.Point(188, 135);
+            this.precipitationLabel.Name = "precipitationLabel";
+            this.precipitationLabel.Size = new System.Drawing.Size(100, 30);
+            this.precipitationLabel.TabIndex = 2;
+            this.precipitationLabel.Click += new System.EventHandler(this.precipitationLabel_Click);
+            // 
+            // highTempLabel
+            // 
+            this.highTempLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.highTempLabel.Location = new System.Drawing.Point(188, 188);
+            this.highTempLabel.Name = "highTempLabel";
+            this.highTempLabel.Size = new System.Drawing.Size(100, 30);
+            this.highTempLabel.TabIndex = 3;
+            // 
+            // lowTempLabel
+            // 
+            this.lowTempLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lowTempLabel.Location = new System.Drawing.Point(188, 238);
+            this.lowTempLabel.Name = "lowTempLabel";
+            this.lowTempLabel.Size = new System.Drawing.Size(100, 30);
+            this.lowTempLabel.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Date:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Precipitation:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(35, 189);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "High Temperature:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(35, 239);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Low Temperature:";
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(188, 369);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 9;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(40, 369);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 10;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Select a Date";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lowTempLabel);
+            this.Controls.Add(this.highTempLabel);
+            this.Controls.Add(this.precipitationLabel);
+            this.Controls.Add(this.dateLabel);
+            this.Controls.Add(this.dateTimePicker);
+            this.Name = "Form1";
+            this.Text = "Weather Data";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label dateLabel;
+        private System.Windows.Forms.Label precipitationLabel;
+        private System.Windows.Forms.Label highTempLabel;
+        private System.Windows.Forms.Label lowTempLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Mahdi Bu-Ali_Lab9_10_164/Mahdi Bu-Ali_Lab9_10_164/WrApp.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+//Mahdi Bu-Ali
+//ECET 16400
+namespace Mahdi_Bu_Ali_Lab11_164
+{
+    public partial class Form1 : Form
+    {
+        //structure for storing data
+        public struct WeatherData
+        {//variable declaration
+            public DateTime date;
+            public double precipitation;
+            public double highTemp;
+            public double lowTemp;
+        }
+        //list of structure
+        List<WeatherData> weatherDataList = new List<WeatherData>();
+        //object instance for structure
+        WeatherData myDataStruct = new WeatherData();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string line = "";//empty string for storing each line
+            //try catch block
+            try
+            {
+                //creating object of StramReader class
+                StreamReader streamReader = new StreamReader("weather.txt");
+                //while is used to read each line from the file
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    //split line on every (;)
+                    string[] tokens = line.Split(';');
+                    //parsing each line and add it into structure
+                    myDataStruct.date = DateTime.Parse(tokens[0]);
+                    myDataStruct.precipitation = double.Parse(tokens[1]);
+                    myDataStruct.highTemp = double.Parse(tokens[2]);
+                    myDataStruct.lowTemp = double.Parse(tokens[3]);
+                    //adding structure to list
+                    weatherDataList.Add(myDataStruct);
+
+                }
+             streamReader.Close();//close the file
+            }
+            //catch exception
+            catch (Exception ex)
+            { //disply exception if any
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            //declarying variables
+            int i = 0;
+            bool date_Found = false;
+            DateTime userSelected = dateTimePicker.Value;//date selected by user
+            while ( (i < weatherDataList.Count) && (date_Found == false))//to check valid date 
+            {
+                var selecteddata = weatherDataList[i];  
+                if (selecteddata.date == userSelected)//incase date found in list
+                {
+                    date_Found = true;//for breaking while loop
+                    //display data in labels
+                    dateLabel.Text = selecteddata.date.ToString("dd/MM/yyyy");
+                    precipitationLabel.Text = selecteddata.precipitation.ToString("");
+                    highTempLabel.Text = selecteddata.highTemp.ToString("");
+                    lowTempLabel.Text = selecteddata.lowTemp.ToString("");
+                }
+                //increment value of i
+                i++;
+                
+            }
+            if (date_Found == false)//incase date is not found
+            {
+                //display empty labels
+                dateLabel.Text = "";
+                precipitationLabel.Text = "";
+                highTempLabel.Text = "";
+                lowTempLabel.Text = "";
+
+            }
+
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();//close application
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            //display empty labels
+            dateLabel.Text = "";
+            precipitationLabel.Text = "";
+            highTempLabel.Text = "";
+            lowTempLabel.Text = "";
+        }
+
+        private void precipitationLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
